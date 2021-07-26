aace583890fb6a8418dddb16a597850858291ca3
Julio Romero <julio_ale21@hotmail.com>
2021-07-26 10:19:26 -0300

    chore: added theme provider and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styles/globals";
+import { theme } from "../theme";
 import client from "../apollo/client";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
