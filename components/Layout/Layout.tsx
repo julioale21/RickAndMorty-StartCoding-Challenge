929cf8602eca8d5c50e78d88eafd9a9debaf5f2f
Julio Romero <julio_ale21@hotmail.com>
2021-07-26 15:11:13 -0300

    chore: added sidebar to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Sidebar } from "../../components";
+import { useMenu } from "../../hooks/useMenu";
 
 const Layout: React.FC = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
+  const node = useRef();
+
+  useMenu(node, () => setOpen(false));
+
   return (
     <>
+      <Sidebar node={node} open={open} setOpen={setOpen} />
       <main>{children}</main>
     </>
   );
