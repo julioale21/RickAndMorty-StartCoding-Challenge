f1aaa555116d0159f094af0c9939f01b2c50363f
Julio Romero <julio_ale21@hotmail.com>
2021-07-26 23:05:13 -0300

    chore: added theme interface

--- theme.ts
+++ theme.ts
@@ -1,4 +1,11 @@
-export const theme = {
+export interface ITheme {
+  primaryDark: string;
+  primaryLight: string;
+  primaryHover: string;
+  mobile: string;
+}
+
+export const theme: ITheme = {
   primaryDark: "#055052",
   primaryLight: "#53B8BB",
   primaryHover: "#003638",
