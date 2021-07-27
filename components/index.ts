f1e6f02e315b75b4dca932b547edeb0ea9e5e996
Julio Romero <julio_ale21@hotmail.com>
2021-07-26 23:06:51 -0300

    chore: exported components

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
-export { default as Layout } from "./Layout";
 export { default as Burger } from "./Burger";
+export { default as Layout } from "./Layout";
 export { default as Menu } from "./Menu";
+export { default as Pagination } from "./Pagination";
 export { default as Sidebar } from "./Sidebar";
