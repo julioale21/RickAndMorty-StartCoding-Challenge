eb5179f2d14c4766fa4201c8a0f33e60e50f869f
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-20 13:04:22 -0300

    styles: updated button styles based on props

--- pages/characters/Characters.styled.ts
+++ pages/characters/Characters.styled.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components";
-import { ITheme } from "../../theme";
+import { ITheme, theme } from "../../theme";
+
+interface IButtonProps {
+  primary?: boolean;
+}
+
+export const Button = styled.button<IButtonProps>`
+  background: ${(props) => (props.primary ? `${theme.primaryLight}` : "white")};
+  border-radius: 10px;
+  border: 2px solid ${theme.primaryLight};
+  color: ${(props) => (props.primary ? "white" : `${theme.primaryLight}`)};
+  cursor: pointer;
+  font-size: 1em;
+  margin: 1em;
+  min-width: 100px;
+  padding: 0.5rem 1rem;
+  :hover {
+    background-color: ${theme.primaryHover};
+  }
+`;
 
 export const Container = styled.div`
   // width: 100vw;
@@ -19,13 +38,15 @@ export const Grid = styled.div<{ theme: ITheme }>`
 `;
 
 export const GridItem = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
-  padding: 10px;
   border: 1px solid gray;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  padding: 10px;
+  box-shadow: 2px 2px 2px ${({ theme }) => theme.primaryLight};
 `;
 
 export const Title = styled.h1`
