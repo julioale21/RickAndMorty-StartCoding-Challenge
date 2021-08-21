3d3fcb0bca5dc65898719ee05cc6f39d303a821c
Julio Romero <julioale21@gmail.com>
2021-08-21 12:56:51 -0300

    style: updated theme

--- theme.ts
+++ theme.ts
@@ -2,12 +2,20 @@ export interface ITheme {
   primaryDark: string;
   primaryLight: string;
   primaryHover: string;
+  secondaryLight: string;
+  secondaryDark: string;
   mobile: string;
+  tablet: string;
+  desktop: string;
 }
 
 export const theme: ITheme = {
   primaryDark: "#055052",
   primaryLight: "#53B8BB",
   primaryHover: "#003638",
+  secondaryLight: "#FFF7AE",
+  secondaryDark: "#F6D167",
   mobile: "576px",
+  tablet: "768px",
+  desktop: "1200px",
 };
