60763e27d5b80d9c6fbdb458ab9977d16850d2dd
Julio Romero <julioale21@gmail.com>
2021-08-21 12:57:28 -0300

    Create Character.styled.ts

---
+++ pages/characters/Character.styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { theme } from "../../theme";
+
+export const VTitle = styled.h1`
+  writing-mode: vertical-rl;
+  font-size: 1rem;
+  color: ${theme.secondaryDark};
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: ${theme.tablet}) {
+    flex-direction: row;
+  }
+`;
+
+export const Image = styled.img`
+  border-radius: 10px;
+  width: 400px;
+  transform: rotate(-5deg);
+`;
