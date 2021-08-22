b1f07a69dd0bb8f9b90448cc7b5f4d7a768964a7
Julio Romero <julioale21@gmail.com>
2021-08-21 22:17:27 -0300

    style: updated 'Menu' component styles

--- components/Menu/Menu.styled.ts
+++ components/Menu/Menu.styled.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components";
-import { ITheme } from "../../theme";
+import { ITheme, theme } from "../../theme";
 
 export const StyledMenu = styled.nav<{ theme: ITheme; open: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background: ${({ theme }) => theme.primaryLight};
+  background: ${theme.primaryDark};
   transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
   height: 100vh;
   text-align: left;
@@ -14,7 +14,7 @@ export const StyledMenu = styled.nav<{ theme: ITheme; open: boolean }>`
   top: 0;
   left: 0;
   transition: transform 0.3s ease-in-out;
-  @media (max-width: ${({ theme }) => theme.mobile}) {
+  @media (max-width: ${theme.mobile}) {
     width: 100%;
   }
   a {
@@ -23,15 +23,15 @@ export const StyledMenu = styled.nav<{ theme: ITheme; open: boolean }>`
     padding: 2rem 0;
     font-weight: bold;
     letter-spacing: 0.5rem;
-    color: ${({ theme }) => theme.primaryDark};
+    color: ${theme.secondaryDark};
     text-decoration: none;
     transition: color 0.3s linear;
-    @media (max-width: ${({ theme }) => theme.mobile}) {
+    @media (max-width: ${theme.mobile}) {
       font-size: 1.5rem;
       text-align: center;
     }
     &:hover {
-      color: ${({ theme }) => theme.primaryHover};
+      color: ${theme.primaryHover};
     }
   }
 `;
