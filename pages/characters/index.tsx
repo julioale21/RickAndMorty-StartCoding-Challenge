c1ef4b667d142ec37f80d1f0a7032ebeb41ced53
Julio Romero <julioale21@gmail.com>
2021-08-21 22:16:44 -0300

    chore: updated characters design

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { FETCH_CHARACTERS } from "../../apollo/queries/characters";
-import { Container, Grid, GridItem, Button, Title } from "./Characters.styled";
+import {
+  Button,
+  CharacterContainer,
+  Grid,
+  GridItem,
+  Image,
+  InfoContainer,
+} from "./Characters.styled";
+
+import { Text } from "../../styles/shared.styled";
 
 const Characters = () => {
   const { data } = useQuery(FETCH_CHARACTERS);
@@ -9,21 +18,29 @@ const Characters = () => {
   if (!data) return "Loading ...";
 
   return (
-    <Container>
-      <Title>Charactes</Title>
+    <CharacterContainer>
+      <Text fontSize="4.5rem" fontWeight="bolder" textShadow="2px 2px 2px white">
+        Characters
+      </Text>
       <Grid>
         {data.characters.results.map((item) => {
           return (
             <GridItem key={item.id}>
-              <img alt="image" src={item.image} />
-              <h2>{item.name}</h2>
-              <p>{item.species}</p>
-              <Button primary>Detail</Button>
+              <Image alt="image" src={item.image} />
+              <InfoContainer>
+                <Text fontSize="1.7rem" fontWeight="bold" marginTop="3rem">
+                  {item.name}
+                </Text>
+                <Text fontSize="1.2rem" margin="0">
+                  {item.species}
+                </Text>
+                <Button primary>View</Button>
+              </InfoContainer>
             </GridItem>
           );
         })}
       </Grid>
-    </Container>
+    </CharacterContainer>
   );
 };
 
