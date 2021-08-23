b5e29f19be09713a50981443e20c20c34c220f2d
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-08-23 12:48:20 -0300

    chore: exported 'Paginator' by default

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
 export { default as Burger } from "./Burger";
 export { default as Layout } from "./Layout";
 export { default as Menu } from "./Menu";
-//export { default as Pagination } from "./Pagination";
+export { default as Paginator } from "./Paginator";
 export { default as Sidebar } from "./Sidebar";
