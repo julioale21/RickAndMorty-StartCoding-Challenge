0d92b29494de933058321e717ad50f2000726f20
Julio Romero <julioale21@gmail.com>
2021-08-23 21:39:27 -0300

    style: added episodes basic styles

---
+++ pages/episodes/Episode.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const EpisodesContainer = styled.div`
+  background-image: url("/rick-and-morty-03.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding-top: 100px;
+  min-height: 100vh;
+`;
+
+export const InfoContainer = styled.div`
+  align-items: center;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
