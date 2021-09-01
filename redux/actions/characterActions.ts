5b50e151f8dd184b4409695265c43a5252bcedc5
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-09-01 10:29:03 -0300

    added actions to set up is loading state and fetch characters

--- redux/actions/characterActions.ts
+++ redux/actions/characterActions.ts
@@ -1,5 +1,6 @@
 import { FETCH_CHARACTERS, SET_ISLOADING } from "./types";
 import { Dispatch } from "redux";
+import CharacterService from "../CharacterService";
 
 const setIsLoading = (isLoading: boolean, dispatch: Dispatch) => {
   dispatch({
@@ -12,4 +13,12 @@ const setIsLoading = (isLoading: boolean, dispatch: Dispatch) => {
 
 export const fetchCharacters = (page?: number) => async (dispatch: Dispatch) => {
   setIsLoading(true, dispatch);
+  CharacterService.fetchCharacters(page).then((data) => {
+    dispatch({
+      type: FETCH_CHARACTERS,
+      payload: { characters: data.characters, info: data.info },
+    });
+
+    setIsLoading(false, dispatch);
+  });
 };
