51bfaa98e9aacc6cc0c303f74ad10425d259e9a6
Julio Romero <julioale21@gmail.com>
2021-09-18 23:05:00 -0300

    fix: fixed export

--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,5 @@ export { default as Layout } from "./Layout";
 export { default as Menu } from "./Menu";
 export { default as Paginator } from "./Paginator";
 export { default as Sidebar } from "./Sidebar";
-export { default as CharacteListSkeleton } from "./skeletons/ListSkeleton";
+export { default as ListSkeleton } from "./skeletons/ListSkeleton";
 export { default as SkeletonItem } from "./skeletons/SkeletonItem";
